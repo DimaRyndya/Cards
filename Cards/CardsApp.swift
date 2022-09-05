ea9743cd5a842d6602ca72dcc9fcd4a5371e5cad
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-09-05 15:36:52 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Dmitriy Ryndya on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
